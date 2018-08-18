12e32fa94c324deed19de75229c494e68bc991f0
abicorios <yaroslav.moroz91@gmail.com>
2018-08-18 14:36:36 -0700

    fix install miniconda3

--- 1adminInstallTools.bat
+++ 1adminInstallTools.bat
@@ -1,5 +1,5 @@
 where ruby /q
 if not %errorlevel%==0 (choco install ruby --x86 -y)
 where conda /q
-if not %errorlevel%==0 (choco install miniconda3 --x86 -y)
+if not %errorlevel%==0 (start https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86.exe)
 exit
