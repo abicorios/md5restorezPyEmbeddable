94e431e68832947a0c665737a0d54f7b90ffa7f2
abicorios <yaroslav.moroz91@gmail.com>
2018-08-19 00:21:52 -0700

    try new test

--- test.rb
+++ test.rb
@@ -3,4 +3,6 @@ if Dir.exist?mto then Dir.rmdir(mto) end
 Dir.mkdir(mto)
 mfrom='C:/Atari Jaguar/GoodJag (2.01)'
 mcsv=Dir['C:/t/Atari*'][0]
-`python md5restorezPyEmbeddable.py --myfrom #{mfrom} --myto #{mto} --mycsv #{mcsv}`
+$LOAD_PATH.push(Dir.pwd)
+require 'myrun.rb'
+myrun("call %USERPROFILE%\Miniconda3\Scripts\activate.bat %USERPROFILE%\Miniconda3 && cell conda activate dev && python md5restorezPyEmbeddable.py --myfrom #{mfrom} --myto #{mto} --mycsv #{mcsv}")
