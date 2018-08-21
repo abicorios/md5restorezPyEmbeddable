341b9279f6025a53b03378d8843d340ea77e23ea
abicorios <yaroslav.moroz91@gmail.com>
2018-08-21 09:52:00 -0700

    fix refresh

--- adminInstallTools.bat
+++ adminInstallTools.bat
@@ -1,6 +1,6 @@
 where choco /q
 if not %errorlevel%==0 (start /wait cmd /c @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin")
-C:\ProgramData\chocolatey\bin\refreshenv
+call C:\ProgramData\chocolatey\bin\refreshenv
 where 7z /q
 if not %errorlevel%==0 (choco install 7zip --x86 -y)
 where git /q
@@ -19,6 +19,10 @@ if not %errorlevel%==0 (start https://repo.continuum.io/miniconda/Miniconda3-lat
 @set a=
 call refreshenv
 call %USERPROFILE%\Miniconda3\Scripts\activate.bat %USERPROFILE%\Miniconda3
-call conda create -n dev python=3 pandas -y
+if not exist %CONDA_PREFIX%\envs\dev call conda create -n dev python=3 pandas -y
+if not exist %CONDA_PREFIX%\envs\exe call conda create -n exe python=3 -y
+call activate exe
+pip install pandas pyinstaller
+echo hiddenimports = ['pandas._libs.tslibs.timedeltas'] > %CONDA_PREFIX%\Lib\site-packages\PyInstaller\hooks\hook-pandas.py
 call deactivate
 call deactivate
