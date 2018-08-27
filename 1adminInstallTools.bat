2b667d98ba789b27cb1c1d591391cc8e82bc33b8
abicorios <yaroslav.moroz91@gmail.com>
2018-08-27 00:47:40 -0700

    rm old bat files

--- 1adminInstallTools.bat
+++
@@ -1,5 +0,0 @@
-where ruby /q
-if not %errorlevel%==0 (choco install ruby --x86 -y)
-where conda /q
-if not %errorlevel%==0 (start https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86.exe)
-exit
