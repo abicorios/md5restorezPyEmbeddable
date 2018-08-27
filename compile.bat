fa6b37b6cc591edd83ff3738438abcd8f7bb973a
abicorios <yaroslav.moroz91@gmail.com>
2018-08-27 02:05:36 -0700

    add compile.bat

---
+++ compile.bat
@@ -0,0 +1,11 @@
+call %USERPROFILE%\scoop\apps\miniconda3\current\Scripts\activate.bat %USERPROFILE%\scoop\apps\miniconda3\current
+call activate exe
+if exist mybuild rmdir mybuild
+mkdir mybuild
+cd mybuild
+copy ..\md5restorezPyEmbeddable.py /y
+pyinstaller -F md5restorezPyEmbeddable.py
+cd ..
+copy mybuild\dist\md5restorezPyEmbeddable.exe /y
+call deactivate
+call deactivate
